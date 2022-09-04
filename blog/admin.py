699685c531a13439c6746d043498b8ade65f971e
mshady nabhan <mshady@asfd.asu.edu.eg>
2022-09-04 10:04:40 +0000

    Generic relation

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Tag
+from .models import Post, Tag, Comment
 # Register your models here.
 
 
@@ -10,3 +10,4 @@ class PostAdmin(admin.ModelAdmin):
 
 admin.site.register(Post,PostAdmin)
 admin.site.register(Tag)
+admin.site.register(Comment)
