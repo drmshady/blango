b745e9f19396eade0d21d6f29bd02500623b9630
mshady nabhan <mshady@asfd.asu.edu.eg>
2022-09-04 09:22:51 +0000

    initial commit

---
+++ blog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Post, Tag
+# Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display=("slug",'published_at')
+    
+
+admin.site.register(Post,PostAdmin)
+admin.site.register(Tag)
